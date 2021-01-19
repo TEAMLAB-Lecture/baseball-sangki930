463128ad7f7f81b35076a0b3a0d206529b17031b
sangkipark <sangki931@gmail.com>
2021-01-19 18:37:06 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -274,8 +274,8 @@ def main():
     # 위의 코드를 포함하여 자유로운 수정이 가능함
     
     while True:
-        user_input=int(input('Input guess number : '))
-        if(user_input==0):
+        user_input=input('Input guess number : ')
+        if(user_input=='0'):
             break
         if is_validated_number(user_input)==False:
             print('Wrong Input, Input again')
